b2881f9aaf3c9d693f321b8751cd1b65c0c139c6
ulil78 <rully.arfan@gmail.com>
2018-03-06 09:22:21 +0700

    Revert "Front"

--- resources/views/front/partials/assets/css.blade.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css">
--- resources/views/front/partials/components/banner/promo.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="jumbotron" id="banner-promo">
-   <h1 class="display-5 text-center">BANNER PROMOTION</h1>
-</div>
