0e1befdadd8be8825167e8f46ebfc1cf2e76daca
ulil78 <rully.arfan@gmail.com>
2018-03-09 08:47:40 +0700

    Remove Menu Hospipital Image in sidebar backend

--- resources/views/backend/components/sidebar.blade.php
+++ resources/views/backend/components/sidebar.blade.php
@@ -157,11 +157,6 @@
                     </li>
                     <li class="nav-item  ">
                         <a href="#" class="nav-link ">
-                            <span class="title">Hospital Images</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="#" class="nav-link ">
                             <span class="title">Hospital Departments</span>
                         </a>
                     </li>
