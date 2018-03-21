c08812de5593e5c47da9627803590888cf4df4ef
ulil78 <rully.arfan@gmail.com>
2018-03-21 15:06:43 +0700

    Edit login admin, merchant

---
+++ resources/views/auth/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light nav_metallic clearfix">
+	<a class="navbar-brand ml-3 text-light align-center float-left" href="#">
+		<img class="img-fluid" src="{{url('img/myworldhealth-logo.png')}}" width="200px" height="200px" class="rounded float-left" alt="...">
+	</a>
+	<ul class="navbar-nav mr-auto mt-2 mt-lg-0 float-left">
+	  <li class="nav-item active">
+	    <a class="nav-link text-light" href="#">
+	        <small>(021) 555 12345</small> <span class="sr-only">(current)</span></a>
+	  </li>
+	</ul>
+	<div class="btn-group my-2 my-lg-0 float-right" role="group" aria-label="Basic example">
+		<a href="#" target="_blank" class="btn btn-outline-light">
+			<i class="ion-social-facebook" style="font-size: 20px;"></i></a>
+		<a href="#" target="_blank" class="btn btn-outline-light">
+			<i class="ion-social-instagram-outline" style="font-size: 20px;"></i></a>
+	</div>
+</nav>
--- resources/views/backend/banner/index.blade.php
+++ resources/views/backend/banner/index.blade.php
@@ -12,29 +12,34 @@
                 <div class="col-md-12">
                   <!-- BEGIN EXAMPLE TABLE PORTLET-->
                     <div class="portlet box green">
-                        <div class="portlet-title">
+                        {{-- <div class="portlet-title">
                             <div class="caption"><i class="fa fa-globe"></i>Banners</div>
                             <div class="tools"></div>
-                        </div>
+                        </div> --}}
                         <div class="portlet-body">
-            						 <table class="table table-striped table-bordered table-hover" id="sample_2">
-            						       <thead>
-            							          <tr>
+
+                          <div class="panel">
+                             <a href="{{url('admin/banners/create')}}" class="btn btn-success">Add Banner</a> 
+                          </div>
+                          <div class="panel-body">
+                         <table class="table table-striped table-bordered table-hover" id="sample_2">
+                               <thead>
+                                    <tr>
                                       <th>No.</th>
-                      								<th>Name</th>
+                                      <th>Name</th>
                                       <th>Url</th>
                                       <th>Position</th>
                                       <th>Picture</th>
                                       <th>Status</th>
-                                      <th> <a href="{{url('admin/banners/create')}}" class="btn btn-primary">Add Banner</a> </th>
+                                      <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                      {{'', $n=1}}
-                    							   @foreach ($banners as $item)
-                    							   <tr>
+                                     @foreach ($banners as $item)
+                                     <tr>
                                         <td>{{$n++}}</td>
-                        								<td>{{$item->name}}</td>
+                                        <td>{{$item->name}}</td>
                                         <td>{{$item->url}}</td>
                                         <td>{{$item->position}}</td>
                                         <td><img height="75" src="{{asset($item->path.$item->filename)}}"></td>
@@ -46,17 +51,41 @@
                                         @endif
                                         </td>
                                         <td>
-                        									  <a href="{{ url('/admin/banners/'.$item->id.'/edit') }}" class="btn btn-warning">EDIT</a>
-                                            <form action="{{ url('admin/banners/'.$item->id) }}" method="POST">
-                                                <input type="hidden" name="_token" value="{{csrf_token()}}" />
-                                                <input type="hidden" name="_method" value="DELETE" />
-                                                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure to delete?')">DELETE</button>
-                                            </form>
+                                            <!-- Action button -->
+                                            <div class="btn-group">
+                                              <button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                Action <span class="caret"></span>
+                                              </button>
+                                              <ul class="dropdown-menu">
+                                                <li>
+                                                  <a href="{{ url('/admin/banners/'.$item->id.'/edit') }}">Edit</a>
+                                                </li>
+                                                <form id="delete-form-{{$item->id}}" 
+                                                    method="post" 
+                                                    action="{{url('admin/banners/'.$item->id) }}"
+                                                    style="display: none;">
+                                                    {{csrf_field()}}
+                                                    {{method_field('DELETE')}}
+                                                </form>
+                                                <li>
+                                                  <a class="dropdown-item" href="" onclick="
+                                                    if(confirm('Are You Sure?')) {
+                                                      event.preventDefault();
+                                                      document.getElementById('delete-form-{{$item->id}}').submit();
+                                                    } else {
+                                                      event.preventDefault();
+                                                    }
+                                                  ">
+                                                  Delete</a>
+                                                </li>
+                                              </ul>
+                                            </div>
                                         </td>
                                       </tr>
                                       @endforeach
                                 </tbody>
                           </table>
+                          </div>
                         </div>
                        <!-- END EXAMPLE TABLE PORTLET -->
                      </div>
